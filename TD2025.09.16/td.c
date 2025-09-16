1dbaf4b55e484db14dc1367919d121c4aa112238
Hasanovic-Hamza <hamza.hasanovic@heig-vd.ch>
2025-09-16 12:35:02 +0000

    added TD20250916

---
+++ TD2025.09.16/td.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_students = 23;
+	//signed int m = -42; // signed opptionel
+    int account_value = -42; // signed opptionel
+	float temperature = 18.1; // 32 bit
+	double pi = 3.1415926; // 64 bit
+}
