244235680bf85f56c12bbd6181ded6fe5c923039
Hasanovic-Hamza <hamza.hasanovic@heig-vd.ch>
2025-10-06 13:31:27 +0000

    added

--- TD2025.09.30/td.c
+++ TD2025.09.30/td.c
@@ -2,31 +2,28 @@
 
 int main(int argc, const char *argv[])
 {
-	
-	double m1 = 0; 										
-	double m2 = 0;
-	double d = 0;
-	const double G = 6.67E-11;
-	
-	printf("Masse 1: ");
-	scanf("%lf", &m1);
-	printf("Masse 2: ");
-	scanf("%lf", &m2);
-	printf("distance: ");
-	scanf("%lf", &d);
+	printf("Number of seconds : ");
+	int s = 0; // nombre de secondes // signed int = int
+	scanf("%d", &s);
 
-	if (m1 <= 0 || m2 <= 0 || d <= 0) // condition 1
-	// bloc 1
-	{		   
-		puts("valeur negative non permise");
+	if (s < 0) // condition 1
+	{		   // bloc 1
+		puts("Error");
 	}
-	else 				// condition 2
-	// bloc 2
-	{
-		double Force = 0;
-		Force  = m1 * m2 * G / (d * d);
+	else if (s >= 86400) // condition 2
+	{					 // bloc 2
+		puts("Overflow");
+	}
+	else // condition 3
+	{	 // bloc 3
+		unsigned int hours = 0;
+		unsigned int minutes = 0;
+		unsigned int secondes = 0;
 
-		printf("%le\n",Force); 
+		hours = s / 3600;
+		minutes = s / 60 - hours * 60;
+		secondes = s - minutes * 60 - hours * 3600;
+
+		printf("%02u:%02u:%02u\n",hours,minutes,secondes); 
 	}
-	
 }
