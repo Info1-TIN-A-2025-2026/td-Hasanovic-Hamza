b3fe96cde64f74bf81ff244b63e8172665117071
Hasanovic-Hamza <hamza.hasanovic@heig-vd.ch>
2025-10-27 15:15:48 +0000

    added

--- TD2025.10.27/td.c
+++ TD2025.10.27/td.c
@@ -1,33 +1,62 @@
-#include <stdio.h> // pour printf et scanf
+//#include <stdio.h> // pour printf et scanf
 
+//int main(int argc, const char *argv[])
+//{
+	// int a = 0;
+	// int b = 0;
+	// int q = 0;
+	// int r = 0;
+
+	// printf("enter a: ");
+	// scanf("%d", &a);
+
+	// printf("enter b: ");
+	// scanf("%d", &b);
+
+	// q = a / b;
+	// r = a % b;
+
+	// printf("a/b = %d\n", q);
+	// printf("a%%b = %d\n", r);
+
+	//int a = 10;
+	//printf("%d\n", a++); // 10
+
+	//a = 10;
+	//printf("%d\n", ++a); // 11
+
+	//char x = 127;
+	//printf("%c\n", ++x); //-128
+
+	//return 0;
+//}
+#include <stdio.h>
+#include <stdbool.h>
 
 int main(int argc, const char *argv[])
 {
-	//int a = 0;
-	//int b = 0;
-	//int q = 0;
-	//int r = 0;
+	const unsigned int I_min = 2;
+	const unsigned int I_max = 100;
 
-	//printf("enter a: ");
-	//scanf("%d", &a);
+	unsigned int i = 0;
 
-	//printf("enter b: ");
-	//scanf("%d", &b);
+	for (i = I_min; i < I_max; i++)
+	{
+		bool is_prime = true;
 
-	//q = a / b;
-	//r = a % b;
-		
-	//printf("a/b = %d\n", q);
-	//printf("a%%b = %d\n", r);
+		unsigned int j = 0;
+		for (j = I_min; j <= i - 1; j++)
+		{
+			if ((i % j) == 0)
+			{
+				is_prime = false;
+			}
+		}
 
-	int a = 10;
-	printf("%d\n", a++); // 10
-
-	a = 10;
-	printf("%d\n", ++a); // 11
-
-	char x = 127;
-	printf("%c\n", ++x); //-128
-
+		if (is_prime)
+		{
+			printf("%d\n", i);
+		}
+	}
 	return 0;
 }
